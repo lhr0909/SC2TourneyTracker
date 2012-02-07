afe227f036831dc5d92b04c9ac0f2025cfd37aab
unknown <mellinja@mellinja-PC.(none)>
2012-02-07 16:54:27 -0500

    stuff

--- web2py/applications/welcome/controllers/tournament.py
+++ web2py/applications/welcome/controllers/tournament.py
@@ -1,5 +1,16 @@
 # coding: utf8
 #
 def index(): 
-    db = DAL('mssql://cheungkt:mechanes5@whale.cs.rose-hulman.edu/SC2tracker')
-    return dict(message="hello from tournament.py")
+    #db = DAL('mssql://cheungkt:mechanes5@whale.cs.rose-hulman.edu/SC2tracker')
+    tournies = db.executesql('SELECT Name, ID FROM Tournament')
+    return dict(tournament = tournies)
+    
+def show():
+    
+    tourny = db.executesql('SELECT * FROM Tournament WHERE ID = ' + str(request.args(0)))
+    
+
+    form = FORM(INPUT(_name = 'Join', _type = 'submit'))
+    if form.accepts(request,session):
+        response.flash = 'Joined the tournament'
+    return dict(test = tourny,form = form)
