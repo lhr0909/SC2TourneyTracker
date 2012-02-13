db7d8c3e6af4cacb8b71696b9b021776de23afc4
unknown <mellinja@mellinja-PC.(none)>
2012-02-12 19:59:17 -0500

    added games page. some junk files were added too

--- web2py/applications/welcome/controllers/tournament.py
+++ web2py/applications/welcome/controllers/tournament.py
@@ -8,9 +8,21 @@ def index():
 def show():
     
     tourny = db.executesql('SELECT * FROM Tournament WHERE ID = ' + str(request.args(0)))
-    
+    form = FORM(INPUT( _type = 'submit',_value="Join"))
+    if form.process().accepted:
+        response.flash = 'Joined the tournament!'
+        pid = db(db.Player.Auth_User_ID == auth.user_id).select()[0]['id']
+        #pid = db.executesql('SELECT ID FROM Player WHERE Auth_User_ID = ' + str(auth.user_id))
+        db.executesql('EXEC [player_to_tourney_sp] ' + str(pid) + ", " + str(request.args(0)))
+    return dict(tourny = tourny,form = form)
 
-    form = FORM(INPUT(_name = 'Join', _type = 'submit'))
-    if form.accepts(request,session):
-        response.flash = 'Joined the tournament'
-    return dict(test = tourny,form = form)
+
+def join():
+    
+    playerID = auth.user.ID
+    tournyID = request.args(0)
+    #TODO: add tourny
+    
+    redirect(URL('show/'+request.args(0)))
+    response.flash = 'Joined the tournament'
+    return dict(pid = request.args(0), message = 'Joined the tournament')
