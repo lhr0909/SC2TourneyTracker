1271a6e62aabae455e17a266cd117e15be8442e4
Simon Liang <no.lhr0909@gmail.com>
2012-02-14 04:29:36 -0500

    edited the files from Jackson.

--- web2py/applications/welcome/controllers/game.py
+++ web2py/applications/welcome/controllers/game.py
@@ -2,7 +2,10 @@
 # try something like
 def index(): 
     if not auth.is_logged_in(): redirect(URL('default','user/login?_next=/' + request.application + '/default/index'))
-    pid = str(db(db.Player.Auth_User_ID == auth.user_id).select()[0]['id'])
-    gids = db.executesql("SELECT GID from Game_to_Players where PID = " + pid)
+    players = db(db.Player.Auth_User_ID == auth.user_id).select()
+    games = []
+    for p in players:
+        pid = p['id']
+        games += db.executesql("SELECT GID from Game_to_Players where PID = " + str(pid))
 
-    return dict(games = gids,pid = pid)
+    return dict(games = games)
